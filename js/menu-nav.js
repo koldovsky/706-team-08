d7162780d7ff481f443476664eaf43e99dc21bc6
S8bik <katryach27@gmail.com>
2022-08-09 10:31:27 +0000

    menu

--- js/menu-nav.js
+++ js/menu-nav.js
@@ -3,18 +3,15 @@
     const menuClose = document.querySelector('.menu-close');
     const menu = document.querySelector('.header__navigation');
     const menuLogo = document.querySelector('.header__container-logo');
-    const bodyLock = document.querySelector('body');
 
     function openMenu(){
         menu.classList.add('active');
         menuLogo.classList.add('active');
-        bodyLock.classList.add('lock');
     }
 
     function closeMenu(){
         menu.classList.remove('active');
         menuLogo.classList.remove('active');
-        bodyLock.classList.remove('lock');
     }
 
     menuBurger.addEventListener('click', openMenu);
